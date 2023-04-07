89031c0c07008eb8fe0aec18b78b097814859ff3
Julian Videla <julipiojo8710@gmail.com>
2023-04-07 19:37:37 -0300

    Se agrega clase Rectangulo

---
+++ src/Figuras/Rectangulo.java
@@ -0,0 +1,18 @@
+package Figuras;
+
+public class Rectangulo {
+
+	private Double ancho;
+	private Double alto;
+	
+	
+	public Rectangulo (Double ancho, Double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+		
+	}
+	
+	
+	
+	
+}
